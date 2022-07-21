7b08f6d44b5e64761baf27fcd5c12aa25fde4dd9
Leonel Mamani <leomamaniinca@frba.utn.edu.ar>
2022-07-21 20:08:24 -0300

    falta agregar color y el label

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ req.onload = function(){
     const xScale = d3.scaleTime()
                      .domain([ dateMin , dateMax ])
                      .range([ padding , w-padding ]);
-    console.log(json)
+    console.log(json.data[0][1])
     const yScale = d3.scaleLinear()
                      .domain([0,d3.max(json.data, d => d[1])])
                      .range([ h-padding , padding ]);
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
+                    
 
 const svg = d3.select('#main')
               .style('background-color','white')
@@ -38,9 +39,32 @@ const svg = d3.select('#main')
     svg.append('g')
        .attr('id','y-axis')
        .attr('transform','translate('+ padding +', 0)')
-       .call(yAxis)
-}
+       .call(yAxis);
 
+    svg.append('text')
+       .text('Gross Domestic Product')
+       .attr('transform','rotate(-90)')
+       .attr('x',-(padding + 180))
+       .attr('y',padding + 20);
+
+    svg.append('text')
+       .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
+       .attr('font-size', '12px')
+       .attr('x', 6*padding)
+       .attr('y', h + 50 - padding);
+
+    svg.selectAll('rect')
+       .data(json.data)
+       .enter()
+       .append('rect')
+       .attr('class','bar')
+       .attr('x',(d,i)=> padding + i*2.985)
+       .attr('y',d=>h-d[1]/70 -padding)
+       .attr('width','2.985')
+       .attr('height',(d)=>{return d[1]/70})
+
+       
+}
 
 const body=d3.select('body')
              .style('background-color','#064D81')
@@ -52,6 +76,7 @@ const body=d3.select('body')
              
 const title = d3.select('#title')
                 .style('margin','0')
+                .style('margin-top','20px')
                 .style('text-align','center');
 
      
